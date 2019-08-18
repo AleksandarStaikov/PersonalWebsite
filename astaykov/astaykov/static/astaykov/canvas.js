1d5077c74ed3ed67bcdc9bc476b5905753c73eff
AleksandarStaikov <13151916+AleksandarStaikov@users.noreply.github.com>
2019-08-18 10:39:17 +0200

    life line event position computation added

--- astaykov/astaykov/static/astaykov/canvas.js
+++ astaykov/astaykov/static/astaykov/canvas.js
@@ -58,22 +58,28 @@ function Event(name, desk, location, position, start, end) {
     this.position = position
     this.start = start
     this.end = end
+    this.x = undefined
 
     this.isSingle = function () {
         return this.end === undefined || this.start === this.end;
     }
 
     this.computeLocation = function () {
-        var availableSpace = cnvas.width;
+        var availableSpace = canvas.width;
+        var daysDiff = Math.floor((eventObjects[eventObjects.length - 1].end - eventObjects[0].start) / (1000 * 60 * 60 * 24));
         
+        var curentDiff = Math.floor((this.start - eventObjects[0].start) / (1000 * 60 * 60 * 24));
 
+        var onePercent = 100 / daysDiff;
+        var curentPercent =  Math.round(curentDiff * onePercent);
+
+        this.x = Math.round((availableSpace - 30) / 100 * curentPercent) + 15 
     }
 
     this.draw = function () {
+        this.computeLocation();
         c.beginPath()
-        c.arc(6, canvas.height / 2, 5, 0, Math.PI * 2, false)
-        //c.fillStyle = randomColor(colors);
-
+        c.arc(this.x, canvas.height / 2, 5, 0, Math.PI * 2, false)
         c.strokeStyle = colors[0];
         c.fillStyle = colors[2];
         c.stroke();
@@ -93,7 +99,9 @@ var readEvents = [];
 var eventObjects = [];
 function init() {
     eventElements.toArray().forEach(a => readEvents.push(parseEvents($(a))));
-    readEvents.forEach(e => eventObjects.push(new Event(e.name, e.description, e.location, e.position, e.start, e.end)));
+    readEvents.forEach(e => eventObjects.push(new Event(e.name, e.description, e.location, e.position, new Date(e.start), new Date(e.end))));
+    eventObjects.push(new Event("Now", null, null, "Right", Date.now(), Date.now()))
+    console.log(eventObjects);
 }
 
 // Animation Loop
