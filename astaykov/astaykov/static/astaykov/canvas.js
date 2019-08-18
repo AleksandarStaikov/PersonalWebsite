af4cfae8000ba7844338ffdc4537fe9d5d0e98ea
AleksandarStaikov <13151916+AleksandarStaikov@users.noreply.github.com>
2019-08-18 15:39:53 +0200

    partly implemented timeframe lifeevent drawing

--- astaykov/astaykov/static/astaykov/canvas.js
+++ astaykov/astaykov/static/astaykov/canvas.js
@@ -50,6 +50,22 @@ function randomColor(colors) {
     return colors[Math.floor(Math.random() * colors.length)]
 }
 
+this.computeLocation = function (date) {
+    if (date === this.undefined || date === null) {
+        return 100;
+    }
+
+    var availableSpace = canvas.width;
+    var daysDiff = Math.floor((eventObjects[eventObjects.length - 1].end - eventObjects[0].start) / (1000 * 60 * 60 * 24));
+
+    var curentDiff = Math.floor((date - eventObjects[0].start) / (1000 * 60 * 60 * 24));
+
+    var onePercent = 100 / daysDiff;
+    var curentPercent = Math.round(curentDiff * onePercent);
+
+    return Math.round((availableSpace - 30) / 100 * curentPercent) + 15
+}
+
 // Objects
 function Event(name, desk, location, position, start, end) {
     this.name = name
@@ -58,33 +74,71 @@ function Event(name, desk, location, position, start, end) {
     this.position = position
     this.start = start
     this.end = end
-    this.x = undefined
+    this.startMark = undefined
+    this.endMark = undefined
 
     this.isSingle = function () {
         return this.end === undefined || this.start === this.end;
     }
 
-    this.computeLocation = function () {
-        var availableSpace = canvas.width;
-        var daysDiff = Math.floor((eventObjects[eventObjects.length - 1].end - eventObjects[0].start) / (1000 * 60 * 60 * 24));
-        
-        var curentDiff = Math.floor((this.start - eventObjects[0].start) / (1000 * 60 * 60 * 24));
 
-        var onePercent = 100 / daysDiff;
-        var curentPercent =  Math.round(curentDiff * onePercent);
-
-        this.x = Math.round((availableSpace - 30) / 100 * curentPercent) + 15 
-    }
 
     this.draw = function () {
-        this.computeLocation();
-        c.beginPath()
-        c.arc(this.x, canvas.height / 2, 5, 0, Math.PI * 2, false)
+        if (this.end !== undefined && +this.end === +this.start) {
+            this.startMark = computeLocation(this.start);
+            this.drawCircle();
+            console.log("c", this.name)
+        } else {
+            this.startMark = computeLocation(this.start);
+            this.endMark = computeLocation(this.end);
+            this.drawTimeFrame();
+            console.log("tf", this.name)
+        }
+    }
+
+    this.drawCircle = function () {
+        c.beginPath();
+        c.arc(this.startMark, canvas.height / 2, 5, 0, Math.PI * 2, false)
         c.strokeStyle = colors[0];
         c.fillStyle = colors[2];
         c.stroke();
         c.fill();
         c.closePath()
+    }
+
+    this.drawTimeFrame = function () {
+        var x = this.startMark;
+        var y = canvas.height / 2;
+        var r = 15;
+        var ld = 60;
+
+        c.beginPath();
+        c.arc(x, y, r, Math.PI, Math.PI * 1.5);
+
+        x += r;
+        y -= r;
+
+        c.lineTo(x + ld, y);
+
+        x += ld;
+        y -= r;
+
+        c.arc(x, y, r, Math.PI * 0.5, 0, true);
+
+        x += 2 * r
+
+        c.arc(x, y, r, Math.PI, Math.PI * 0.5, true);
+
+        x += r + ld
+        y += r
+
+        c.lineTo(x, y)
+
+        y += r;
+
+        c.arc(x, y, r, Math.PI * 1.5, Math.PI * 0);
+
+        c.stroke();
     }
 
     this.update = function () {
